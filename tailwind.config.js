70f25b0d8aa6142517c56418217f83b4ed2a3eb7
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2024-05-17 09:13:38 +0100

    Config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./views/**/*.twig"
-  ],
+  content: ["./views/**/*.twig"],
   theme: {
     fontFamily: {
-      sans: ['nimbus_sans_lregular']
+      sans: ["nimbus_sans_lregular"],
     },
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     fontSize: {
-      xs: ['0.6875rem', '1.25rem'], // 13px
-      sm: ['0.875rem', '1.25rem'], // 13px
-      base: ['1rem', '1.5rem'], // 16px, 24px
-      md: ['1.125rem', '1.75rem'], // 18px, 28px
-      lg: ['1.562rem', '2.44rem'], // 20px, 32px
-      'xl': ['1.938rem', '2.25rem'], // 31px, 36px
-      '2xl': ['2.438rem', '2.75rem'], // 39px, 44px
-      '3xl': ['3.062rem', '3.5rem'],
-    },
-    colors: {
-      'blue': '#2530CE',
-      'black': '#2E2C2C'
+      xs: ["0.6875rem", "1.25rem"], // 10px
+      sm: ["0.875rem", "1.25rem"], // 14px, 20px
+      base: ["1rem", "1.5rem"], // 16px, 24px
+      md: ["1.125rem", "1.75rem"], // 18px, 28px
+      lg: ["1.5625rem", "2.25rem"], // 25px, 36px
+      xl: ["1.9375rem", "2.625rem"], // 31px, 42px
+      "2xl": ["2.4375rem", "3rem"], // 39px, 48px
+      "3xl": ["3.0625rem", "3.75rem"], // 49px, 48px
     },
     extend: {
       colors: {
-        clifford: '#da373d',
-      }
-    }
+        blue: "#2F50F4",
+        "blue-dark": "#0F0F79",
+        black: "#2E2C2C",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
