9079abae52666110ec2455c72357dab725cfa499
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2024-05-22 08:11:41 +0100

    Fixing team members hp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 import Glide from '@glidejs/glide'
-const g = new Glide('.glide', {
-  perView: 2,
-  focusAt: 'center',
-  breakpoints: {
-    1024: {
-      perView: 1
-    }
-  }
-}).mount()
 
+if(document.querySelector('.glide')){
+  new Glide('.glide', {
+    perView: 2,
+    focusAt: 'center',
+    breakpoints: {
+      1280: {
+        perView: 1
+      }
+    }
+  }).mount()
+}
 console.log('Little Fish Lab')
