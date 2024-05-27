b53262d5dc4f065b0f93fd58bcd012a421e067a4
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2024-05-27 18:20:51 +0100

    Instant page load and animating anchor page scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,8 @@ const initGlide = (el) => {
   }).mount()
 };
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Init glide carousel, if element is present
-  const glideEl = document.querySelector('.glide')
-  if(glideEl){
-    initGlide(glideEl);
-  }
 
-  // Init mobile nav
+const initMobileNav = () => {
   const navM = document.querySelector(".lfl-js-nav-mobile");
   const navMOpen = navM.getElementsByTagName("button")[0];
   const navMOverlay = navM.querySelector(".lfl-js-nav-mobile__overlay");
@@ -36,5 +30,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
     document.body.style.overflow = "visible"; 
     navMOpen.setAttribute("aria-expanded", "false")
   })
+}
+
+// Function to handle link clicks
+function handleLinkClick(event) {
+  if (event.target.tagName === 'A' && event.target.hash) {
+      // Check if the link points to a different page
+      const href = event.target.getAttribute('href');
+      if (href && href.includes('#')) {
+          event.preventDefault();
+
+          // Store the hash in sessionStorage
+          sessionStorage.setItem('targetHash', event.target.hash);
+
+          // Navigate to the target page
+          window.location.href = href.split('#')[0];
+      }
+  }
+}
+
+// Function to handle hash change
+function handleHashChange(hash) {
+  try {
+      if (hash && hash !== "#" && hash !== "") {
+          // console.log("Hashchange - " + hash);
+
+          // Jump to section with id of hash
+          let target = document.querySelector(hash);
+          if (target) {
+              let offset = 80;
+              scrollTo(target, offset);
+          }
+      }
+  } catch (e) {
+      console.error('Could not handle hash change: ' + hash);
+  }
+}
+
+// Function to smoothly scroll to an element
+function scrollTo(element, offset = 0, duration = 800) {
+  // console.log('scrolling:');
+  // console.log(element != null);
+
+  if (element) {
+    let targetPosition = element.getBoundingClientRect().top + window.pageYOffset - offset;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Init glide carousel, if element is present
+  const glideEl = document.querySelector('.glide')
+  if(glideEl){
+    initGlide(glideEl);
+  }
+
+  // Init mobile nav
+  initMobileNav();
+  document.body.addEventListener('click', handleLinkClick);
+
+
+  const targetHash = sessionStorage.getItem('targetHash');
+  if (targetHash) {
+      sessionStorage.removeItem('targetHash'); // Clear the stored hash
+      handleHashChange(targetHash); // Handle the stored hash
+  }
 
 });
