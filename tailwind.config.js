27aef3faf10844fcee0837e0adeb0b24daef6a52
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2024-06-13 11:48:08 +0100

    Homepage carousel image height

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     screens: {
       wp_admin: "782px",
+      xs: "440px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
@@ -36,9 +37,6 @@ module.exports = {
         "blue-700": "#050577",
         black: "#000",
         "gray-dark": "#4D5149"
-      },
-      screens: {
-        'xs': '512px'
       },
       borderRadius: {
         'xl': '1rem',
